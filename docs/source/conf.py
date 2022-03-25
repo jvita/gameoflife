7179b8180f709a75d7f9e6276b094dc91eaec021
Josh <vita.joshua@gmail.com>
2022-03-25 11:19:14 -0500

    Rebuild for GH pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autosectionlabel',
     'sphinx_copybutton',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
